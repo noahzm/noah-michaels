e2d1445e34823312d909adfefafd903fef252e46
Noah Michaels <2554594+nhmchls@users.noreply.github.com>
2021-11-10 08:59:48 -0500

    Updated colors, disabled dark mode, modified copy on About page

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,7 +18,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <SimpleReactLightbox>
-        <div className="flex flex-col min-h-screen justify-between dark:bg-gray-900 bg-gray-100">
+        <div className="flex flex-col min-h-screen justify-between dark:bg-gray-900">
           <Navbar siteTitle={data.site.siteMetadata?.title || `Title`} />
           <main className="text-xl lg:text-3xl container m-auto px-4 pb-4 text-gray-900 dark:text-gray-100">
             {children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: "media",
+  //darkMode: "media",
   theme: {
     fontFamily: {
       sans: ["futura-pt"],
